792b84344da968bba39b1015d652a534d33f76d7
Trevor WM <Trevormoore.2016@gmail.com>
2023-09-04 18:46:30 -0700

    basic components

---
+++ src/components/home.tsx
@@ -0,0 +1,104 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <div>
+        Huitzilin Transformational Life Coaching
+      </div>
+      <div>
+        Helping people become their best Self regardless of life's challenges
+      </div>
+      <div>
+        <img src={'https://res.cloudinary.com/dfrj03hsi/image/upload/v1693878067/Huitzilin/166FE277-043C-40C3-820C-1E8CC74A8D26rg7_0881_li3l0z.jpg'} alt='preview' />
+      </div>
+      <div>
+        {"Welcome to Huitzilin Transformational Life Coaching! \
+        My name is Angela and I am a transformation coach. \
+        For almost two decades I have coached people through \
+        acting, singing, writing, and simplifying cluttered \
+        offices, homes, and minds. The common result in the \
+        coaching I’ve provided has been transformation. \
+        I’ve been blessed to witness my clients gain the capacity, \
+        confidence, and courage to live life on their own terms. \
+        Read on to see if working with me is right for you, \
+        then you can schedule a call here: \
+        https://calendly.com/colibritlc/30min"}
+      </div>
+      <div>
+        <ul> Who I Help
+          <li>
+            Those with the desire to become their best Self, regardless of life’s challenges.
+          </li>
+          <li>
+            I have coached creatives, professionals, entrepreneurs, students, and parents.
+          </li>
+          <li>
+            Although I do not coach people who are in active substance misuse, active suicidal ideation, or in active domestic violence, I have coached those in recovery.
+          </li>
+        </ul>
+        <ul> What you can expect from working with me
+          <li>
+            Healthier communication, relating, and boundary skills.
+          </li>
+          <li>
+            Healthier relationships, with yourself, others, community, and nature.
+          </li>
+          <li>
+            Increased self-confidence, self-trust, and self-worth.
+          </li>
+          <li>
+            A multitude of tools depending on your needs, such as drawing, writing, somatic, and visualization exercises.
+          </li>
+          <li>
+            Lightness, laughter, and appreciation regardless of the issues that arise.
+          </li>
+        </ul>
+        <ul> How coaching differs from therapy/psychiatry
+          <li>
+            I do NOT diagnose, treat, or prescribe.
+          </li>
+          <li>
+            Although I specialize in coaching people through trauma recovery, I will refer out if a client would be better helped by another professional. Your health and safety are my priority.
+          </li>
+          <li>
+            I DO help you uncover what you actually want, become aware of unconscious resistance, and transform the parts of you that are holding you back, into powerful allies that will help you reach your goals. You already have within you everything you need, I am simply the midwife who helps you bring your dreams to life.
+          </li>
+        </ul>
+        <ul> What I offer
+          <li>
+            One-to-one coaching video-chats that last 55 minutes, for 12 consecutive weeks.
+          </li>
+          <li>
+            Messaging support between calls.
+          </li>
+        </ul>
+        <ul> My experience
+          <li>
+            I studied at the International Association of Trauma Recovery Coaches, and am continuing my education in this emerging field that is bringing relief and hope to so many.
+          </li>
+          <li>
+            I have successfully coached people who live with symptoms of ADHD, anxiety, body dysmorphia and disordered eating, complex PTSD, depression, divorce, grief, narcissistic, financial, and sexual abuse, and those going through life-transitions.
+          </li>
+          <li>
+            My personal lived experience recovering from my own traumas and navigating this world as a queer, biracial, neurodivergent woman has taught me to have the courage to seek help, develop resilience, and grow the capacity to thrive. I have developed the compassion, patience, and ability it takes to be on this journey, and I am honored to share what I’ve learned with those who choose to share their journey with me.
+          </li>
+        </ul>
+        <ul> What clients are saying
+          <li>
+            “Angela provides a safe space where I don't feel judged or looked down upon. She gives truths in a way that’s manageable, and lets me work at my own pace. I have grown and become stronger, and so have my relationships. I’ve even noticed family and friends grow through sharing what I have learned in my sessions with Angela. Sharing her own growth gives me hope and motivation to continue growing.” ~S
+          </li>
+          <li>
+            “Angela is incredibly present and validating, and is always pointing out and celebrating my victories. She brings all her real-world experience in navigating trauma in a very down to earth real way, and brings psycho-education and hope. Angela was there for me in my grief like no one else was. She knew how to sit with me without fixing, and knew what to say when most didn't. Angela always raises my vibe and helps me feel empowered and hopeful.” ~T
+          </li>
+          <li>
+            “Angela addressed my trauma and how it affects my decision making. She helped me learn how to recognize when it causes anxiety or “gloom” thinking, and offered tools to work with it or through it.” ~Kimmi
+          </li>
+        </ul>
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
